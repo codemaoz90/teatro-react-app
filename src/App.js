fe591f913a906d173d1531c12e9dca76e3b2cea5
codemaoz90 <codemaoz@gmail.com>
2021-02-23 03:04:02 +0100

    Niveles culminados 1,2,3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import jsonLocal from "./data.json";
+import { useState } from "react";
+import styled from "styled-components";
+const Container = styled.div`
+	display: flex;
+	width: 100vw;
+	height: 100vh;
+	justify-content: center;
+	align-items: center;
+`;
+const Button = styled.button`
+	font-size: 1.5em;
+	width: 50%;
+	text-align: center;
+	color: gray;
+`;
+const Paragraph = styled.p`
+	background: pink;
+`;
+export default function App() {
+	const [contador, setContador] = useState(0);
+	console.log(typeof contador);
+	const data = jsonLocal.map((dato, index) =>
+		contador === index ? (
+			<Paragraph key={index}>{dato}</Paragraph>
+		) : (
+			<p key={index}>{dato}</p>
+		)
+	);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container>
+			<div style={{ width: "500px" }}>
+				<Button
+					onClick={() =>
+						setContador(contador <= 0 ? jsonLocal.length - 1 : contador - 1)
+					}
+				>
+					PREV
+				</Button>
+				<Button
+					onClick={() =>
+						setContador(contador >= jsonLocal.length - 1 ? 0 : contador + 1)
+					}
+				>
+					NEXT
+				</Button>
+				<br />
+				{data}
+				{/* <p>{jsonLocal[contador]}</p> */}
+			</div>
+		</Container>
+	);
 }
-
-export default App;
